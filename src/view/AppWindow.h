1f4b75a8e5a3acdb8ebde1707e20c4e31c62d81a
Loïc Le Page <llepage@neodesys.com>
2015-09-23 20:15:36 +0200

    Quick and dirty import of AppWindow_Win32
    - code broken
    - must be cleaned and adpated

--- src/view/AppWindow.h
+++ src/view/AppWindow.h
@@ -27,6 +27,12 @@
 
 #include <cstddef>
 
+//TODO: AppWindow_Win32 import, clean and adapt!
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif //_WIN32
+
 #include "../utils/Logger.h"
 
 namespace utils
@@ -54,7 +60,30 @@ namespace view
 			return s_uniqueInstance;
 		}
 
+		/**
+		 * Creates and displays the application window on screen.
+		 * @param x the initial horizontal position of the upper-left corner of
+		 * the window in screen coordinates (default is 0).
+		 * @param y the initial vertical position of the upper-left corner of
+		 * the window in screen coordinates (default is 0).
+		 * @param w the initial width of the window client area. If negative or
+		 * null, the window will be created with a default geometry of 320x240
+		 * (default value).
+		 * @param h the initial height of the window client area. If negative or
+		 * null, the window will be created with a default geometry of 320x240
+		 * (default value).
+		 * @param name the window name or nullptr if no name (default value).
+		 * @param icon the window icon or nullptr if no icon (default value).
+		 * @param bResizable pass true if the window is resizable (default
+		 * value), else pass false.
+		 * @return true on success or false on error.
+		 */
 		bool create(int x = 0, int y = 0, int w = 0, int h = 0, const char* name = nullptr, const utils::Icon* icon = nullptr, bool bResizable = true);
+
+		/**
+		 * Destroys the application window and releases any resources
+		 * associated with it.
+		 */
 		void destroy();
 
 		bool isResizable() const
@@ -62,31 +91,78 @@ namespace view
 			return m_bResizable;
 		}
 
+		/**
+		 * @return true if the cursor is showing while hovering over the window
+		 * client area or false if not.<br>
+		 * By default, cursor is not visible.
+		 */
 		bool isCursorVisible() const
 		{
 			return m_bCursorVisible;
 		}
 
+		/**
+		 * @param bVisible pass true to show the cursor while hovering over the
+		 * window client area, or pass false to hide the cursor.
+		 * @return true on success or false on error.
+		 */
 		bool showCursor(bool bVisible);
 
+		/**
+		 * @return true if the window is in full-screen mode, else returns
+		 * false if it is in normal windowed mode.<br>
+		 * By default, window is in normal windowed mode.
+		 */
 		bool isFullscreen() const
 		{
 			return m_bFullscreen;
 		}
 
+		/**
+		 * Switches the window to full-screen or windowed mode.
+		 * @param bFullscreen pass true to go full-screen or false to get back
+		 * to normal windowed mode.
+		 * @return true on success or false on error.
+		 */
 		bool setFullscreen(bool bFullscreen);
 
+		/**
+		 * @return true if the window is activated (normal state), or false if
+		 * not (iconic state).<br>
+		 * You must call dispatchWindowsEvents() function regularly
+		 * to maintain this information up-to-date.<br>
+		 * As a general matter, drawing to the window should be paused while
+		 * the window is not activated.
+		 */
 		bool isActivated() const
 		{
 			return m_bActivated;
 		}
 
+		/**
+		 * @return the current size of the window client area.<br>
+		 * Once the window has been created, it is guaranteed that returned
+		 * width and height are both superior or equal to 1.
+		 * If the window has not been created yet, returned width and height
+		 * are both null.<br>
+		 * You must call dispatchWindowsEvents() function regularly to maintain
+		 * this size up-to-date.
+		 */
 		const Size& getSize() const
 		{
 			return m_size;
 		}
 
+		/**
+		 * Adds a window event listener to the window.
+		 * @param l the window event listener to add.
+		 */
 		void addWindowEventListener(IWindowEventListener& l);
+
+		/**
+		 * Removes a window event listener from the window.
+		 * @param l the window event listener to remove.
+		 */
 		void removeWindowEventListener(IWindowEventListener& l);
 
 		size_t getWindowEventListenerCount() const
@@ -94,6 +170,18 @@ namespace view
 			return m_windowEventListenerCount;
 		}
 
+		/**
+		 * Window events dispatching method.<br>
+		 * It must be called regularly in order to correctly dispatch all
+		 * events to the application window.<br>
+		 * In particular, window current size and state are updated during the
+		 * event loop.
+		 * @return true if the application window is still valid, or false if
+		 * the application window has been destroyed or if the program has been
+		 * killed by an external signal.<br>
+		 * As a general matter, if this function returns false, the program
+		 * should terminate.
+		 */
 		bool dispatchWindowsEvents();
 
 	private:
@@ -124,6 +212,11 @@ namespace view
 		void fireButtonReleasedEvent(unsigned int button, int x, int y);
 		void fireMouseWheelEvent(bool bUp, int x, int y);
 		void firePointerMovedEvent(int x, int y);
+
+		//TODO: AppWindow_Win32 import, clean and adapt!
+#ifdef _WIN32
+		friend LRESULT CALLBACK windowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+#endif //_WIN32
 	};
 }
 
--- src/view/AppWindow_Win32.cpp
+++ src/view/AppWindow_Win32.cpp
@@ -19,25 +19,409 @@
 
 #ifdef _WIN32
 
+//TODO: AppWindow_Win32 quick and dirty import from another project, must be cleaned and adapted
+
 #include "AppWindow.h"
 
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
+#include <csignal>
+
+#include <windowsx.h>
 #include <GL/gl.h>
 
 #include "../utils/Image.h"
 
+#ifndef ENDSESSION_CLOSEAPP
+#define ENDSESSION_CLOSEAPP 0x1
+#endif //!ENDSESSION_CLOSEAPP
+
+namespace
+{
+	class DisplayDelegate final
+	{
+	public:
+		DisplayDelegate();
+		~DisplayDelegate();
+
+		bool createAppWindow(int x, int y, unsigned int uWidth, unsigned int uHeight, const char* name, const utils::Icon* icon, bool bResizable);
+		void destroyAppWindow();
+
+		bool showCursor(bool bVisible) const;
+		bool setFullscreen(bool bFullscreen) const;
+
+		inline bool mustQuit() const
+		{
+			return s_signalQuit;
+		}
+
+		static inline void signalQuit()
+		{
+			s_signalQuit = 1;
+		}
+
+	private:
+		DisplayDelegate(const DisplayDelegate&) = delete;
+		DisplayDelegate& operator=(const DisplayDelegate&) = delete;
+
+		static const utils::Logger s_logger;
+
+		static volatile sig_atomic_t s_signalQuit;
+		static BOOL WINAPI signalInterception(DWORD dwCtrlType);
+
+		ATOM m_wndClassAtom = 0;
+		HWND m_hAppWindow = NULL;
+		HDC m_hDC = NULL;
+		HGLRC m_hGLCtx = NULL;
+
+		bool m_bTrackingMouse = false;
+
+		bool initWindowClass();
+	} s_displayDelegate;
+
+	DisplayDelegate::DisplayDelegate()
+	{
+		SetConsoleCtrlHandler(&DisplayDelegate::signalInterception, TRUE);
+	}
+
+	DisplayDelegate::~DisplayDelegate()
+	{
+		destroyAppWindow();
+	}
+
+	const utils::Logger DisplayDelegate::s_logger("view::AppWindow_Win32");
+
+	volatile sig_atomic_t DisplayDelegate::s_signalQuit = 0;
+
+	BOOL WINAPI DisplayDelegate::signalInterception(DWORD)
+	{
+		s_signalQuit = 1;
+		return TRUE;
+	}
+
+	bool DisplayDelegate::createAppWindow(int x, int y, unsigned int uWidth, unsigned int uHeight, const char* name, const utils::Icon* icon, bool bResizable)
+	{
+		//TODO
+		if (m_hAppWindow || !initWindowClass())
+			return false;
+
+		RECT wndRect = {x, y, x + (int)uWidth, y + (int)uHeight};
+		DWORD wndStyle = WS_OVERLAPPEDWINDOW;
+		DWORD wndExStyle = WS_EX_APPWINDOW | WS_EX_WINDOWEDGE;
+
+		wndStyle |= WS_CLIPSIBLINGS | WS_CLIPCHILDREN;
+		AdjustWindowRectEx(&wndRect, wndStyle, FALSE, wndExStyle);
+
+		HWND hWnd = CreateWindowEx(wndExStyle, reinterpret_cast<LPCSTR>(m_wndClassAtom), name, wndStyle, wndRect.left, wndRect.top, wndRect.right - wndRect.left, wndRect.bottom - wndRect.top, NULL, NULL, GetModuleHandle(NULL), NULL);
+		if (!hWnd)
+			return false;
+
+		HDC hDC = GetDC(hWnd);
+		if (!hDC)
+		{
+			DestroyWindow(hWnd);
+			return false;
+		}
+
+		static const PIXELFORMATDESCRIPTOR pfd = {
+			sizeof(PIXELFORMATDESCRIPTOR), 1,
+			PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,
+			PFD_TYPE_RGBA,
+			32,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			24,
+			8,
+			0, PFD_MAIN_PLANE, 0, 0, 0, 0
+		};
+
+		int pixFormat = ChoosePixelFormat(hDC, &pfd);
+		if (!pixFormat || !SetPixelFormat(hDC, pixFormat, &pfd))
+		{
+			ReleaseDC(hWnd, hDC);
+			DestroyWindow(hWnd);
+			return false;
+		}
+
+		m_hGLCtx = wglCreateContext(hDC);
+		if (!m_hGLCtx)
+		{
+			ReleaseDC(hWnd, hDC);
+			DestroyWindow(hWnd);
+			return false;
+		}
+
+		m_hDC = hDC;
+		m_hAppWindow = hWnd;
+
+		ShowWindow(hWnd, SW_SHOW);
+		SetForegroundWindow(hWnd);
+		SetFocus(hWnd);
+
+		return true;
+	}
+
+	void DisplayDelegate::destroyAppWindow()
+	{
+		m_bTrackingMouse = false;
+
+		if (m_hGLCtx)
+		{
+			wglMakeCurrent(NULL, NULL);
+			wglDeleteContext(m_hGLCtx);
+			m_hGLCtx = NULL;
+		}
+
+		if (m_hDC)
+		{
+			if (m_hAppWindow)
+				ReleaseDC(m_hAppWindow, m_hDC);
+
+			m_hDC = NULL;
+		}
+
+		if (m_hAppWindow)
+		{
+			DestroyWindow(m_hAppWindow);
+			m_hAppWindow = NULL;
+
+			ReleaseCapture();
+			ClipCursor(NULL);
+			ShowCursor(true);
+		}
+
+		if (m_wndClassAtom)
+		{
+			UnregisterClass(reinterpret_cast<LPCSTR>(m_wndClassAtom), NULL);
+			m_wndClassAtom = 0;
+		}
+	}
+
+	bool DisplayDelegate::showCursor(bool bVisible) const
+	{
+		//TODO
+		if (m_hAppWindow)
+		{
+			if (bVisible)
+			{
+				if (m_bTrackingMouse)
+				{
+					m_bTrackingMouse = false;
+					ShowCursor(true);
+
+					TRACKMOUSEEVENT tme = {};
+					tme.cbSize = sizeof(tme);
+					tme.dwFlags = TME_CANCEL | TME_LEAVE;
+					tme.hwndTrack = m_hAppWindow;
+					TrackMouseEvent(&tme);
+				}
+
+				s_logger.info("cursor shown");
+			}
+			else
+			{
+				//Generate a WM_MOUSEMOVE event if cursor is within this
+				//window client area.
+				POINT p = {};
+				if (GetCursorPos(&p))
+					SetCursorPos(p.x, p.y);
+
+				s_logger.info("cursor hidden");
+			}
+
+			return true;
+		}
+		else
+			s_logger.error("cannot change cursor visibility, application window doesn't exist");
+
+		return false;
+	}
+
+	bool DisplayDelegate::setFullscreen(bool bFullscreen) const
+	{
+		//TODO
+		return false;
+	}
+
+	bool DisplayDelegate::initWindowClass()
+	{
+		if (m_wndClassAtom)
+			return true;
+
+		WNDCLASSEX wc = {};
+		wc.cbSize = sizeof(wc);
+		wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+		wc.lpfnWndProc = (WNDPROC)&view::windowProc;
+		wc.hInstance = GetModuleHandle(NULL);
+		wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+		wc.hCursor = (HCURSOR)LoadImage(NULL, IDC_ARROW, IMAGE_CURSOR, 0, 0, LR_SHARED | LR_DEFAULTSIZE);
+		wc.lpszClassName = "DeluxeReader";
+
+		m_wndClassAtom = RegisterClassEx(&wc);
+		if (m_wndClassAtom)
+			return true;
+		else
+			return false;
+	}
+}
+
 namespace view
 {
+	LRESULT CALLBACK windowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		view::AppWindow& wnd = view::AppWindow::getAppWindow();
+		switch (uMsg)
+		{
+		case WM_PAINT:
+			wnd.fireRepaintEvent();
+			break;
+
+		case WM_ACTIVATE:
+			if (wnd.isFullscreen())
+			{
+				if (LOWORD(wParam) == WA_INACTIVE)
+					ShowWindow(hWnd, SW_MINIMIZE);
+				else
+				{
+					//TODO: restore full-screen window on further activation
+				}
+			}
+			return 0;
+
+		case WM_WINDOWPOSCHANGED:
+			{
+				const WINDOWPOS* pWndPos = reinterpret_cast<const WINDOWPOS*>(lParam);
+				if (pWndPos)
+				{
+					if (IsIconic(hWnd) || !IsWindowVisible(hWnd))
+						wnd.fireStateChangedEvent(false);
+					else
+					{
+						if (!(pWndPos->flags & SWP_NOSIZE))
+							wnd.fireSizeChangedEvent(pWndPos->cx, pWndPos->cy);
+
+						wnd.fireStateChangedEvent(true);
+					}
+				}
+			}
+			return 0;
+
+		case WM_KEYDOWN:
+		case WM_SYSKEYDOWN:
+			wnd.fireKeyPressedEvent(wParam);
+			return 0;
+
+		case WM_KEYUP:
+		case WM_SYSKEYUP:
+			wnd.fireKeyReleasedEvent(wParam);
+			return 0;
+
+		case WM_LBUTTONDOWN:
+			wnd.fireButtonPressedEvent(1, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+			return 0;
+
+		case WM_LBUTTONUP:
+			wnd.fireButtonReleasedEvent(1, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+			return 0;
+
+		case WM_MBUTTONDOWN:
+			wnd.fireButtonPressedEvent(2, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+			return 0;
+
+		case WM_MBUTTONUP:
+			wnd.fireButtonReleasedEvent(2, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+			return 0;
+
+		case WM_RBUTTONDOWN:
+			wnd.fireButtonPressedEvent(3, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+			return 0;
+
+		case WM_RBUTTONUP:
+			wnd.fireButtonReleasedEvent(3, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+			return 0;
+
+		case WM_MOUSEWHEEL:
+			{
+				short delta = GET_WHEEL_DELTA_WPARAM(wParam);
+				POINT pos = {GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam)};
+				if (ScreenToClient(hWnd, &pos))
+				{
+					if (delta > 0)
+						wnd.fireMouseWheelEvent(true, pos.x, pos.y);
+					else if (delta < 0)
+						wnd.fireMouseWheelEvent(false, pos.x, pos.y);
+				}
+			}
+			return 0;
+
+		//TODO
+		/*
+		case WM_MOUSELEAVE:
+			if (wnd.m_bTrackingMouse)
+			{
+				wnd.m_bTrackingMouse = false;
+
+				if (!wnd.m_bCursorVisible && !wnd.isFullScreen())
+					ShowCursor(true);
+			}
+			return 0;
+
+		case WM_MOUSEMOVE:
+			if (!wnd.m_bCursorVisible && !wnd.isFullScreen() && !wnd.m_bTrackingMouse)
+			{
+				TRACKMOUSEEVENT tme = {};
+				tme.cbSize = sizeof(tme);
+				tme.dwFlags = TME_LEAVE;
+				tme.hwndTrack = hWnd;
+				if (TrackMouseEvent(&tme))
+				{
+					wnd.m_bTrackingMouse = true;
+					ShowCursor(false);
+				}
+			}
+
+			wnd.firePointerMovedEvent(GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+			return 0;
+		*/
+
+		case WM_CLOSE:
+			wnd.fireClosingEvent();
+			return 0;
+
+		case WM_ENDSESSION:
+			if ((lParam & ENDSESSION_CLOSEAPP) && !wParam)
+				return 0;
+			else
+				DisplayDelegate::signalQuit();
+			return 0;
+
+		case WM_SYSCOMMAND:
+			if (((wParam == SC_SCREENSAVE) || (wParam == SC_MONITORPOWER)) && wnd.isFullscreen())
+				return 0;
+			break;
+		}
+
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	}
+
 	bool AppWindow::create(int x, int y, int w, int h, const char* name, const utils::Icon* icon, bool bResizable)
 	{
-		//TODO: implement Win32 Window create
-		return false;
+		if ((w < 1) || (h < 1))
+		{
+			w = 320;
+			h = 240;
+		}
+
+		if (s_displayDelegate.createAppWindow(x, y, w, h, name, icon, bResizable))
+		{
+			m_bResizable = bResizable;
+			return true;
+		}
+		else
+			return false;
 	}
 
 	void AppWindow::destroy()
 	{
-		//TODO: implement Win32 Window destroy
+		fireStateChangedEvent(false);
+		s_displayDelegate.destroyAppWindow();
 		m_bResizable = true;
 		m_bCursorVisible = true;
 		m_bFullscreen = false;
@@ -47,20 +431,56 @@ namespace view
 
 	bool AppWindow::showCursor(bool bVisible)
 	{
-		//TODO: implement Win32 Window showCursor
-		return false;
+		if (bVisible != m_bCursorVisible)
+		{
+			if (!s_displayDelegate.showCursor(bVisible))
+				return false;
+
+			m_bCursorVisible = bVisible;
+		}
+
+		return true;
 	}
 
 	bool AppWindow::setFullscreen(bool bFullscreen)
 	{
-		//TODO: implement Win32 Window setFullscreen
-		return false;
+		if (!m_bResizable)
+		{
+			s_logger.error("cannot change fullscreen state, application window is not resizable");
+			return false;
+		}
+
+		if (bFullscreen != m_bFullscreen)
+		{
+			if (!s_displayDelegate.setFullscreen(bFullscreen))
+				return false;
+
+			m_bFullscreen = bFullscreen;
+		}
+
+		return true;
 	}
 
 	bool AppWindow::dispatchWindowsEvents()
 	{
-		//TODO: implement Win32 Window dispatchWindowsEvents
-		return false;
+		MSG msg;
+		for (;;)
+		{
+			if (s_displayDelegate.mustQuit())
+				return false;
+
+			if (!PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+				return true;
+
+			if (msg.message == WM_QUIT)
+			{
+				DisplayDelegate::signalQuit();
+				return false;
+			}
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
 	}
 }
 
